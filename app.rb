2cf5a3cbca6024bca0c3f9e41dc51b4b8468f6ec
Dmitriy Khait <khaitdmitriy@gmail.com>
2016-08-23 11:52:45 -0400

    Done.

--- app.rb
+++ app.rb
@@ -2,5 +2,23 @@ require 'sinatra/base'
 
 class App < Sinatra::Base
 
+ get "/newteam" do
+
+     erb :newteam
+
+ end
+
+ post "/newteam" do
+   @tm = params["name"]
+   @co = params["coach"]
+   @pg = params["pg"]
+   @s = params["sg"]
+   @p = params["pf"]
+    @sf = params["sf"]
+     @c = params["c"]
+
+   erb :team
+
+ end
 
 end
